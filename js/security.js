f72e07e9b8df99c4f95296bc5ceca1f8e5992d50
TAYTISCHA <87822758+TAYTISCHA@users.noreply.github.com>
2026-05-28 02:11:42 +0700

    Update security.js

--- js/security.js
+++ js/security.js
@@ -1,1 +1,35 @@
+function getDeviceId(){
 
+  let deviceId = localStorage.getItem('deviceId');
+
+  if(!deviceId){
+
+    deviceId = crypto.randomUUID();
+
+    localStorage.setItem(
+      'deviceId',
+      deviceId
+    );
+  }
+
+  return deviceId;
+}
+
+document.addEventListener('contextmenu', e=>{
+  e.preventDefault();
+});
+
+document.addEventListener('keydown', e=>{
+
+  if(
+    e.key === 'F12' ||
+    (
+      e.ctrlKey &&
+      e.shiftKey &&
+      e.key === 'I'
+    )
+  ){
+    e.preventDefault();
+  }
+
+});
