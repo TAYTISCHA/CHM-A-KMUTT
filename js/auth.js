b232af95c05ed3d2fcff4a33827a7186250d3f99
TAYTISCHA <87822758+TAYTISCHA@users.noreply.github.com>
2026-05-28 02:12:00 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,73 @@
+async function login(){
 
+  const studentId =
+    document.getElementById('studentId').value;
+
+  const password =
+    document.getElementById('password').value;
+
+  const res = await fetch(API_URL,{
+    method:'POST',
+    body:JSON.stringify({
+      action:'login',
+      studentId,
+      password,
+      deviceId:getDeviceId()
+    })
+  });
+
+  const data = await res.json();
+
+  const msg =
+    document.getElementById('message');
+
+  if(data.success){
+
+    localStorage.setItem(
+      'token',
+      data.token
+    );
+
+    localStorage.setItem(
+      'role',
+      data.role
+    );
+
+    window.location.href='dashboard.html';
+
+  }else{
+
+    msg.innerText=data.message;
+
+  }
+}
+
+async function register(){
+
+  const studentId =
+    document.getElementById('studentId').value;
+
+  const password =
+    document.getElementById('password').value;
+
+  const inviteCode =
+    document.getElementById('inviteCode').value;
+
+  const res = await fetch(API_URL,{
+    method:'POST',
+    body:JSON.stringify({
+      action:'register',
+      studentId,
+      password,
+      inviteCode,
+      deviceId:getDeviceId()
+    })
+  });
+
+  const data = await res.json();
+
+  const msg =
+    document.getElementById('message');
+
+  msg.innerText=data.message;
+}
