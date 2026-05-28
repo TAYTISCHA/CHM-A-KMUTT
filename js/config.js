2f106c4b443cc96ce144cc1fe68673d50c53b261
TAYTISCHA <87822758+TAYTISCHA@users.noreply.github.com>
2026-05-29 02:24:47 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbyE5AGanT3Pcg___5X6htoy7W2gOCvJYgLQ_wrKlOAa9iXCBzunKR6DIBGFXAg5Q2i1nw/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyhpTFeKUxAlL2hvoIpRDufhrkrQ8NoCnzv-ctuJPq2tJpaKIrRtUmbD2jVDifqa3yH/exec';
